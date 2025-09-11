e4686f969d1d0fe68e4b921b899e514b217a4f78
Felipe M <philbone@focused.cl>
2025-09-10 21:17:02 -0300

    Agrega Diagrama UML del ejercicio TortasArtesanales

--- TortasArtesanales/Main.java
+++ TortasArtesanales/Main.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
-public class Main {
+public abstract class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Cliente cliente = new Cliente("1-9", "Juan Pérez", 70, "juan@mail.com");
-        Pastelero pastelero = new Pastelero("2-7", 1, "Ana López", "20200101", "decoración temática");
+        Cliente cliente = new Cliente("1456764-9", "Juan Pérez", 80, "juan@mail.com");
+        Pastelero pastelero = new Pastelero("24654844-7", 1, "Ana López", "20200101", "decoración temática");
         Torta torta = TortaFactory.crearTorta(1, "Torta Cumpleaños", "12 porciones", "Chocolate", "Personalizada");
 
         List<DescuentoStrategy> descuentos = Arrays.asList(new DescuentoEdad(), new DescuentoMonto());
